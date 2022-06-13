3dcfeb75cbdec62fe7ff71c3ddd528e6a39cab11
DiegoUser <garcia_mendez_diego@hotmail.com>
2022-06-13 11:33:14 -0300

    agregamos el tipo de data

--- Pages/ProcesoINS.php
+++ Pages/ProcesoINS.php
@@ -5,52 +5,28 @@ $descripcion = utf8_decode($_POST["DES"]);
 $importe     = $_POST["IMP"];
 $fecha       = $_POST["FEC"];
 $tipo        = $_POST["Añadir"];
+$tipoGasto   = '';
 
 //definir tipo
-if ($tipo=1||$tipo=2) {
-    // crear sentencia SQL
-    $sql  = "INSERT INTO gestion ";
-    $sql .= "(idGastos,descripcionGastos,importeGastos,fechaGastos,tipoGastos) ";
-    $sql .= "VALUES ";
-    $sql .= "(null,'$descripcion','$importe','$fecha','Ingreso')";
-
-// depurar SQL
-// die($sql);
-// ejecutar sentencia SQL
-mysqli_query($conex, $sql);
-// cerrar conexión
-mysqli_close($conex);
-// volver al forumulario automáticamente
-header("Location: catalogo.php");
-}elseif ($tipo=3||$tipo=4) {
-    // crear sentencia SQL
-    $sql  = "INSERT INTO gestion ";
-    $sql .= "(idGastos,descripcionGastos,importeGastos,fechaGastos,tipoGastos) ";
-    $sql .= "VALUES ";
-    $sql .= "(null,'$descripcion','$importe','$fecha','Egreso')";
-
-// depurar SQL
-// die($sql);
-// ejecutar sentencia SQL
-mysqli_query($conex, $sql);
-// cerrar conexión
-mysqli_close($conex);
-// volver al forumulario automáticamente
-header("Location: catalogo.php");
-}elseif ($tipo=5) {
-    // crear sentencia SQL
-    $sql  = "INSERT INTO gestion ";
-    $sql .= "(idGastos,descripcionGastos,importeGastos,fechaGastos,tipoGastos) ";
-    $sql .= "VALUES ";
-    $sql .= "(null,'$descripcion','$importe','$fecha','Ahorro')";
-
-// depurar SQL
-// die($sql);
-// ejecutar sentencia SQL
-mysqli_query($conex, $sql);
-// cerrar conexión
-mysqli_close($conex);
-// volver al forumulario automáticamente
-header("Location: catalogo.php");
+if ($tipo == '1' || $tipo == '2') {
+    $tipoGasto = 'Ingreso';
+} elseif ($tipo == '3' || $tipo == '4') {
+    $tipoGasto = 'Egreso';
+} elseif ($tipo == '5') {
+    $tipoGasto = 'Ahorro';
 }
 
+// crear sentencia SQL
+$sql  = "INSERT INTO gestion ";
+$sql .= "(idGastos,descripcionGastos,importeGastos,fechaGastos,tipoGastos) ";
+$sql .= "VALUES ";
+$sql .= "(null,'$descripcion','$importe','$fecha','$tipoGasto')";
+
+// depurar SQL
+// die($sql);
+// ejecutar sentencia SQL
+mysqli_query($conex, $sql);
+// cerrar conexión
+mysqli_close($conex);
+// volver al forumulario automáticamente
+header("Location: catalogo.php");
